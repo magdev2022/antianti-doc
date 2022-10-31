deab49ce6493b8332ad74f88eefcda42ef7252ad
unknown <jin1991919@hotmail.com>
2022-10-31 18:38:43 +0900

    change title

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title} Manual`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
